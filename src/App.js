93bf5e200960e7cd5eeb66d2bef5ffed2ef2d9a4
Surachet Thongprasong <surachet.th@kkumail.com>
2022-12-03 12:29:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import ice from '../src/img/ice.jpg'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div>
+        <ul className='uuuu'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+
+      </div>
+
+      <div>
+
+        <img src={ice} className="logonav" alt="surachet" ></img>
+      </div>
+
     </div>
+
+
   );
 }
 
